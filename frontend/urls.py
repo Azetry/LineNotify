f951d39f9ba8930e2cd510b6f9edd490fbbf6238
Azetry <azetry.bt10@nycu.edu.com>
2022-10-19 05:22:59 +0800

    access token

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'frontend'
 urlpatterns = [
-    path('test', views.authorize, name='test'),
+    path('test', views.login, name='test'),
     path('login/callback', views.callback, name='callback')
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,8 +1,7 @@
 from http import client
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
-import os
-import random, string
+import os, random, string, requests
 
 # Functions
 def randStr(digits=8):
@@ -14,14 +13,9 @@ def randStr(digits=8):
     '''
     return ''.join(random.choice(string.ascii_letters + string.digits) for x in range(digits))
 
-# Create your views here.
-def test(request):
-    return HttpResponse("Hello")
 
-
-# Line Login
-def authorize(request):
-    ''' Line Login authorization (code-flow)
+def authorizeUrl():
+    ''' Return Line Login authorization (code-flow) url
     '''
     client_id = os.getenv('LOGIN_CLIENT')
     redirect_uri = "https://azetry-linenotify.herokuapp.com/login/callback"
@@ -32,8 +26,41 @@ def authorize(request):
     auth_url = "https://access.line.me/oauth2/v2.1/authorize"
     auth_url = auth_url + \
                 f"?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&state={state}&scope={scope}&nonce={nonce}"
+    return auth_url
+
+
+def accessToken(code):
+    ''' Access bearer token'''
+
+    token_url = "https://api.line.me/oauth2/v2.1/token"
+    params = {
+        'code': code,
+        'grant_type': "authorization_code",
+        'redirect_uri': "https://azetry-linenotify.herokuapp.com/login/callback",
+        'client_id': os.getenv("LOGIN_CLIENT"),
+        'client_secret': os.getenv("LOGIN_SECRET")
+    }
+    res = requests.post(token_url, json = params)
+
+
+    return res
+
+# Create your views here.
+def test(request):
+    return HttpResponse("Hello")
+
+
+# Line Login
+def login(request):
+    auth_url = authorizeUrl()
+
     return redirect(auth_url)
 
 
+
 def callback(request):
-    return JsonResponse(request.GET.dict())
+    res = request.GET.dict()
+
+    infos = accessToken(res['code'])
+    
+    return JsonResponse(infos)
